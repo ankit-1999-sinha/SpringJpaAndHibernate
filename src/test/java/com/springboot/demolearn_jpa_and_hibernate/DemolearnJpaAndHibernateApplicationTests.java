26f9d4c2afdac0d12752b1d92691c2d6be9d9b59
ankit-1999-sinha <ankitecesinha@gmail.com>
2024-10-09 19:17:13 +0530

    first commit

---
+++ src/test/java/com/springboot/demolearn_jpa_and_hibernate/DemolearnJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demolearn_jpa_and_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolearnJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
